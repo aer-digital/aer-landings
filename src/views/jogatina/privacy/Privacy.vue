a32b1a0e13841f31126e172796953f7bac470a61
Rapha Ueda <rueda@iportsolutions.com.br>
2026-04-01 16:02:41 -0300

    .

--- src/views/jogatina/privacy/Privacy.vue
+++ src/views/jogatina/privacy/Privacy.vue
@@ -4,6 +4,5 @@
 
 <script setup lang="ts">
 import PDF from 'pdf-vue3'
-
-const pdfUrl = `${import.meta.env.BASE_URL}docs/privacy.pdf`
+import pdfUrl from '@/assets/docs/privacy.pdf'
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import { dirname, resolve } from 'node:path'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [vue()],
-  // base: './', // Configura caminhos relativos para o Amplify
-  base: '/aer-landings/', // Configura caminhos relativos para o GitHub Pages
+  base: '/aer-landings/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        'jogatina': resolve(__dirname, 'src/views/jogatina/index.html'),
+        jogatina: resolve(__dirname, 'src/views/jogatina/index.html'),
         'jogatina-privacy': resolve(__dirname, 'src/views/jogatina/privacy/index.html'),
       }
     },
-    outDir: 'dist',       // Ativa a geração na pasta dist
-    emptyOutDir: true,    // Limpa a pasta antes de cada build
-    assetsInlineLimit: 0  // Garante que todas as imagens sejam copiadas
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 0
+  }
 })
